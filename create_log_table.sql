0e2a155668ea4f848af997202a3e4ca64b9471b0
MirroAi <mirrorai@qq.com>
2019-08-11 15:56:08 +0800

    增加字段，方便理解

--- create_log_table.sql
+++ create_log_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE `logs`(
     `id` INT PRIMARY KEY AUTO_INCREMENT COMMENT '日志id',
     `count` INT NOT NULL COMMENT '第几次执行',
     `test_case_id` INT NOT NULL COMMENT '测试用例id',
+    `test_case_name` VARCHAR ( 255 ) NOT NULL COMMENT '测试用例名',
     `is_success` INT NOT NULL COMMENT '是否成功，成功 1，失败 0',
     `create_at` DATETIME NOT NULL COMMENT '记录时间'
 )ENGINE=INNODB CHARSET=utf8mb4 COMMENT '执行日志表';
